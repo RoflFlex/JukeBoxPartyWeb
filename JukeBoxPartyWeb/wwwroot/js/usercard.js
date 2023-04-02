00ba886e289445af9573ad8eea7dcb623ca746ac
RoflFlex <61744245+RoflFlex@users.noreply.github.com>
2023-04-02 22:14:06 +0200

    Styling

--- JukeBoxPartyWeb/wwwroot/js/usercard.js
+++ JukeBoxPartyWeb/wwwroot/js/usercard.js
@@ -11,36 +11,18 @@
     }
     connectedCallback() {
         this.render();
+        this.applyStyling();
     }
 
     applyStyling() {
         const linkElem = document.createElement("link");
         linkElem.setAttribute("rel", "stylesheet");
-        linkElem.setAttribute("href", "/css/usercard.css");
+        linkElem.setAttribute("href", "../css/usercard.css");
         this.shadow.appendChild(linkElem);
     }
     render() {
         this.shadow.innerHTML = `
-            <style>
-                p {
-                    font-size:15px;
-                    margin: 0;
-                }
-                img {
-                    width: 40px;
-                }
-                div {
-                    padding: 6px;
-                    border-radius: 10px;
-                    border-style: solid;
-                    border-color: aqua;
-                    border-width: 3px;
-                    margin-top: 3px;
-                    margin-left: 2px;
-                    margin-right: 2px;
-                    margin-bottom: 3px;
-                }
-            </style>
+           
             <div class="usercard">
                 <img class="userpic" src="${this.url}">
                 <p class="nickname">${this.name}</p>
