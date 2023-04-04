24ba6d1d71267eafa0f414dd3320c781242ede60
RoflFlex <61744245+RoflFlex@users.noreply.github.com>
2023-04-04 11:12:28 +0200

    Sendgrid

--- JukeBoxPartyWeb/Services/AuthMessageSenderOptions.cs
+++ JukeBoxPartyWeb/Services/AuthMessageSenderOptions.cs
@@ -2,6 +2,6 @@
 {
     public class AuthMessageSenderOptions
     {
-        public string? SendGridKey { get; set; }
+        public const string SendGridKey = "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";
     }
 }
